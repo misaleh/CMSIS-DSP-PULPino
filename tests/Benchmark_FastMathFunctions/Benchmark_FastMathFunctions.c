59e4b5e6f15792aeecb881db2f7f07554555c14f
Mostafa Saleh <moustafa.i.saleh@gmail.com>
2017-06-29 18:48:33 +0200

    optimized GPIO control to remove overhead from benchmarking for FastMath and other changes

--- tests/Benchmark_FastMathFunctions/Benchmark_FastMathFunctions.c
+++ tests/Benchmark_FastMathFunctions/Benchmark_FastMathFunctions.c
@@ -6,7 +6,21 @@
 #include "string_lib.h"
 #include "bar.h"
 
-#define PRINT_OUTPUT  /*for testing functionality for each function, removed while benchmarking*/
+//#define PRINT_OUTPUT  /*for testing functionality for each function, removed while benchmarking*/
+/*
+  this macros used for benchmarking, they are not friendly as they affect other GPIOs, but the main purpose here is to minimize the overhead,
+a more fierndly version will be
+#define SET_GPIO_6()     *(volatile int*) (GPIO_REG_PADOUT) |=  0x20;
+#define CLR_GPIO_6()    *(volatile int*) (GPIO_REG_PADOUT) &=  0xDF;
+#define SET_GPIO_5()     *(volatile int*) (GPIO_REG_PADOUT) |=  0x10;
+#define CLR_GPIO_5()     *(volatile int*) (GPIO_REG_PADOUT) &=  0xEF;
+*/
+
+#define SET_GPIO_6()     *(volatile int*) (GPIO_REG_PADOUT) =  0x20;
+#define CLR_GPIO_6()    *(volatile int*) (GPIO_REG_PADOUT) =  0x00;
+#define SET_GPIO_5()     *(volatile int*) (GPIO_REG_PADOUT) =  0x10;
+#define CLR_GPIO_5()     *(volatile int*) (GPIO_REG_PADOUT) =  0x00;
+
 /*
 *Each function has a GPIO pin (5 and 6 alternatively ) set before it runs and is cleared after it finish running
 to measure the time of execution of each function.
@@ -26,6 +40,9 @@ int angle =360;
 volatile float32_t test_f= 4.2;
 volatile q15_t test_q15 = 0x1245;
 volatile q31_t test_q31 = 0x48A3617B;
+volatile float32_t test_angle_f32 = 0.9;
+volatile q15_t test_angle_q15 = 0x0FD6;
+volatile q31_t test_angle_q31 = 0x07FFFFFF;
 int32_t main(void)
 {
 
@@ -34,14 +51,14 @@ int32_t main(void)
   set_gpio_pin_direction(5, DIR_OUT);
   set_pin_function(6, FUNC_GPIO);
   set_gpio_pin_direction(6, DIR_OUT);
-  set_gpio_pin_value(5, 0);
-  set_gpio_pin_value(6, 0);
+  CLR_GPIO_5();
+  CLR_GPIO_6();
 /*Tests*/
 /*sqrt*/
   test_f +=2.0;
-  set_gpio_pin_value(6, 1);	
+  SET_GPIO_6();	
   riscv_sqrt_f32(test_f,&result_f32);
-  set_gpio_pin_value(6, 0);
+  CLR_GPIO_6();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_sqrt_f32:\n");  
   printf("%d ",(int)(result_f32*100));  
@@ -49,18 +66,18 @@ int32_t main(void)
 
 
 #endif
-  set_gpio_pin_value(5, 1);
-  riscv_sqrt_q15(0x1245,&result_q15);
-  set_gpio_pin_value(5, 0);
+  SET_GPIO_5();
+  riscv_sqrt_q15(test_q15,&result_q15);
+  CLR_GPIO_5();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_sqrt_q15:\n");  
   printf("0x%X ",result_q15);  
   printf("\n");
   printf("Correct answer = 0x305B \n");
 #endif
-  set_gpio_pin_value(6, 1);	
-  riscv_sqrt_q31(0x48A3617B,&result_q31);
-  set_gpio_pin_value(6, 0);
+  SET_GPIO_6();	
+  riscv_sqrt_q31(test_q31,&result_q31);
+  CLR_GPIO_6();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_sqrt_q31:\n");  
   printf("0x%X ",result_q31);  
@@ -68,25 +85,25 @@ int32_t main(void)
   printf("Correct answer = 0x606CAE24 \n");
 #endif
 /*cos*/
-  set_gpio_pin_value(5, 1);	
-  result_f32=riscv_cos_f32(0.9);
-  set_gpio_pin_value(5, 0);
+  SET_GPIO_5();	
+  result_f32=riscv_cos_f32(test_angle_f32);
+  CLR_GPIO_5();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_cos_f32:\n");  
   printf("%d ",(int)(result_f32*100));  
 #endif
-  set_gpio_pin_value(6, 1);	
-  result_q15=riscv_cos_q15(0x0FD6);
-  set_gpio_pin_value(6, 0);
+  SET_GPIO_6();	
+  result_q15=riscv_cos_q15(test_angle_q15);
+  CLR_GPIO_6();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_cos_q15:\n");  
   printf("0x%X ",result_q15);  
   printf("\n");
   printf("Correct answer = 0x5B3A \n");
 #endif
-  set_gpio_pin_value(5, 1);
-  result_q31=riscv_cos_q31(0x07FFFFFF);
-  set_gpio_pin_value(5, 0);
+  SET_GPIO_5();
+  result_q31=riscv_cos_q31(test_angle_q31);
+  CLR_GPIO_5();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_cos_q31:\n");  
   printf("0x%X ",result_q31); 
@@ -94,25 +111,25 @@ int32_t main(void)
   printf("Correct answer = 0x7641AF3C \n"); 
 #endif
 /*sin*/
-  set_gpio_pin_value(6, 1);	
-  result_f32=riscv_sin_f32(0.9);
-  set_gpio_pin_value(6, 0);
+  SET_GPIO_6();	
+  result_f32=riscv_sin_f32(test_angle_f32);
+  CLR_GPIO_6();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_sin_f32:\n");  
   printf("%d ",(int)(result_f32*100));  
 #endif
-  set_gpio_pin_value(5, 1);	
-  result_q15=riscv_sin_q15(0);
-  set_gpio_pin_value(5, 0);
+  SET_GPIO_5();	
+  result_q15=riscv_sin_q15(test_angle_q15);
+  CLR_GPIO_5();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_sin_q15:\n");  
   printf("0x%X ",result_q15); 
   printf("\n");
-  printf("\nCorrect answer = 0 \n"); 
+  printf("\nCorrect answer = 0x59C4 \n"); 
 #endif
-  set_gpio_pin_value(6, 1);
-  result_q31=riscv_sin_q31(0x07FFFFFF);
-  set_gpio_pin_value(6, 0);
+  SET_GPIO_6();
+  result_q31=riscv_sin_q31(test_angle_q31);
+  CLR_GPIO_6();
 #ifdef PRINT_OUTPUT
   printf("\nriscv_sin_q31:\n");  
   printf("0x%X ",result_q31);  
